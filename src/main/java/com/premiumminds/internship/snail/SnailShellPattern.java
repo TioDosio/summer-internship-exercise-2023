3a34122cb053db5d4e0dcc0db67880e1d87fb497
André Teodósio <andre.teodosio@tecnico.ulisboa.pt>
2023-06-01 15:59:36 +0100

    Teste com 6x6, algoritmo feito

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -1,5 +1,6 @@
 package com.premiumminds.internship.snail;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
 /**
@@ -9,21 +10,56 @@ class SnailShellPattern implements ISnailShellPattern {
 
   /**
    * Method to get snailshell pattern
-   * 
+   *
    * @param matrix matrix of numbers to go through
    * @return order array of values thar represent a snail shell pattern
    */
   public Future<int[]> getSnailShell(int[][] matrix) {
-    //throw new RuntimeException("Not Implemented Yet");
-    int n = matrix.length;
-    int[] result = new int[n * n];
-    for(int i=0; i<n; i++) {
-      for(int j=0; j<n; j++) {
-
-      }
+    CompletableFuture<int[]> result = new CompletableFuture<>();
+    if(matrix.length == 0) {
+      result.complete(new int[0]);
+      return result;
     }
+      int rows = matrix.length;
+      int cols = matrix.length;
+      int[] snailShell = new int[rows*cols];
+      int index = 0;
+      int rowInit = 0;
+      int rowFin = rows - 1;
+      int colInit = 0;
+      int colFin = cols - 1;
+      int i=0;
+
+      while (rowInit <= rowFin && colInit <= colFin) {
+          // Traverse the top row
+          for (i = colInit; i <= colFin; i++) {
+              snailShell[index++] = matrix[rowInit][i];
+          }
+          rowInit++;
+          // Traverse the right column
+          for (i = rowInit; i <= rowFin; i++) {
+              snailShell[index++] = matrix[i][colFin];
+          }
+          colFin--;
+          // Traverse the bottom row
+          if (rowInit <= rowFin) {
+              for (i = colFin; i >= colInit; i--) {
+                  snailShell[index++] = matrix[rowFin][i];
+              }
+              rowFin--;
+          }
+
+          // Traverse the left column
+          if (colInit <= colFin) {
+              for (i = rowFin; i >= rowInit; i--) {
+                  snailShell[index++] = matrix[i][colInit];
+              }
+              colInit++;
+          }
+      }
+      result.complete(snailShell);
+      return result;
 
 
-    return null;
   };
 }
